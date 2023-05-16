d8ed5e8b67363092b442f3362f40df4e34e634af
ahilacondo <ahilacondo@unsa.edu.pe>
2023-05-16 15:08:58 -0500

    corrijiendo Funcion arrayGenerator

--- ejer1.js
+++ ejer1.js
@@ -1,12 +1,21 @@
-let x= arrayGenerator(11,1,6)
-
+/*let x= arrayGenerator(11,1,6)
 
 function arrayGenerator(n, min, max){
     let a = [];
     for(let x = 0; x < n ; x++){
         a.push(Math.ceil(Math.random() * (max -min) + min));
     }
-    return a
+    return a;
 }
 
-console.log(x)
+console.log(x)*/
+
+
+function arrayGenerator(n, min, max) {
+    return new Array(n).fill(0).map(function () {
+        return Math.floor(Math.random() * (max - min) + min);
+    });
+  }
+  
+  const randomArray = arrayGenerator(5, 1, 10);
+  console.log(randomArray);
